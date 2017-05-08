a6dd380e3321bcc18c7b9b6a9de4919013f842b8
Selva <k.n.selvakumar@gmail.com>
2017-05-08 15:50:47 +0530

    Use std::function for the CallbackFunction

--- src/CallbackFunction.h
+++ src/CallbackFunction.h
@@ -1,8 +1,8 @@
 #ifndef CALLBACKFUNCTION_H
 #define CALLBACKFUNCTION_H
- 
+
 #include <Arduino.h>
 
-typedef void (*CallbackFunction) ();
+typedef std::function<void(void)> CallbackFunction;
 
 #endif
